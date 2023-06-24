37f932c052eed26cd6ecb00a270d5ebd847d3c7f
Victor Hugo <victorhugoraj@gmail.com>
2023-06-24 20:46:44 -0300

    Criando modificacao de script de configuracao

--- start.sh
+++ start.sh
@@ -8,12 +8,18 @@ echo "--------------------------------------------------------------------------
 
 read -p "Do you want to start the universe astro? (Y/N)" CONFIRMATION
 if [ $CONFIRMATION = "y" ] ; then
-    echo "=> Starting universe ASTRO<=" 
+    echo "=> Starting universe ASTRO <=" 
     echo "=> Stop all container <=" 
     docker-compose stop
+    echo "=> Install node modules for services <="
+    for pathService in `ls ./Services`
+        do
+            $(cd Services/$pathService && npm install) > /dev/null 2>&1
+        done
+    echo "=> DONE - Installed node modules <="
     echo "=> Start all containers <="
     docker-compose up --build -d
-fi
 
-echo "" 
-echo "=> Started universe ASTRO <="
+    echo "" 
+    echo "=> Started universe ASTRO <="
+fi
